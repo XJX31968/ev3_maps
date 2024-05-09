9fef5a9239c643d1f0d9459cd94cc8216b0e723d
pablo <pablo@DESKTOP-3AKGU8K>
2024-05-09 11:11:19 +0200

    2 commit

--- src/main/Start.java
+++ src/main/Start.java
@@ -15,7 +15,7 @@ public class Start {
 	public static void main(String[] args) {
 		
 		
-		HashMap <String,Coche> coches = new HashMap<String, Coche>();
+		HashMap <Persona,Coche> coches = new HashMap<Persona, Coche>();
 		
 		Coche c1 = new Coche("1111BBB", "Rojo", "Mercedes");
 		Coche c2 = new Coche("1111CCC", "Verde", "Mercedes");
@@ -26,17 +26,19 @@ public class Start {
 		Persona p2 = new Persona("71742170Y","Pepe");
 		
 		
-		coches.put(c1.getMatricula(), c1);
-		coches.put(c2.getMatricula(), c2);
-		coches.put(c3.getMatricula(), c3);
-		coches.put(c4.getMatricula(), c4);
+		coches.put(p1, c1);
+		coches.put(p1, c2);
+		coches.put(p2, c3);
+		coches.put(p2, c4);
 		
-		Set<String> keys = coches.keySet();
+		System.out.println(coches);
+		
+		/*Set<String> keys = coches.keySet();
 		for(String k:keys) {
 			
 			//System.out.println(k+" = "+coches.get(k));
 		}
-		
+		*/
 		Collection<Coche> losCoches =  coches.values();
 		
 		List<Coche> listado = new ArrayList<Coche>();
